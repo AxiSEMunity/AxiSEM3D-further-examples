a161bcd1634e2b96955af569849d41034789db3d
Andrew Walker <andrew.walker@earth.ox.ac.uk>
2022-04-21 16:47:53 +0100

    Setup and run selected jobs
    These need to run in /work...

--- run_examples.sh
+++ run_examples.sh
@@ -9,18 +9,21 @@ Help()
 	echo "options:"
 	echo " a    run all known examples"
 	echo " r    update and rebuild AxiSEM3d"
+	echo " d    dryrun (don't actually submit jobs)"
 	echo "Otherwise the example in each example_dir is submitted to the queue"
 }
 
 # Main script
 rebuild=false
 all=false
+dryrun=false
 while [ $# -gt 0 ]; do
-    while getopts arh name; do
+    while getopts arhd name; do
         case $name in
             a) all=true;;
             r) rebuild=true;;
             h) Help;;
+	    d) dryrun=true;;
         esac
     done
     [ $? -eq 0 ] || exit 1
@@ -45,8 +48,21 @@ if [ $all = "true" ]; then
 else
 	EXAMPLES=${ARGS[*]}
 fi
- 
-for EXAMPLE in ${EXAMPLES}; do
-	echo "Not running " $EXAMPLE
 
+BINDIR=/home/n03/n03/andreww/AxiSEM-3D-test/build
+RUNDIR=/work/n03/n03/andreww/axisem3d-examples-test
+mkdir -p $RUNDIR
+
+for EXAMPLE in ${EXAMPLES}; do
+	mkdir -p $RUNDIR/${EXAMPLE##*/}
+	cp -r $EXAMPLE/input $RUNDIR/${EXAMPLE##*/}
+	cp $BINDIR/axisem3d $RUNDIR/${EXAMPLE##*/}
+	cp axisem3d_sub.sh $RUNDIR/${EXAMPLE##*/}
+	cd $RUNDIR/${EXAMPLE##*/}
+	if [ $dryrun != "true" ]; then
+		sbatch axisem3d_sub.sh
+	else
+		echo "Not running " $EXAMPLE " but set up"
+	fi
+	cd -
 done
