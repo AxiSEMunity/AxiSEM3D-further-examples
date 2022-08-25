46f0f41163e380818172b872fe6f27d687e8d232
Andrew Walker <andrew.walker@earth.ox.ac.uk>
2022-08-25 16:15:23 +0100

    Start to unpick the three examples in example 3
    If this works I'll do the other two.

---
+++ examples/05_01_anisotropy_global_fullCij_5s/jobparams.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This file contains per-job parameters needed to run the
+# particular example on ARCHER2
+
+AX3D_JOB_NAME=axisem3d-example05_01
+AX3D_JOB_NODES=4
+AX3D_JOB_TASKS_PER_NODE=128
+AX3D_JOB_CPUS_PER_TASK=1
+AX3D_JOB_TIME=24:00:00
